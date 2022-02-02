c6f72a403101c36903b2408456d2ab085d7da68e
brianjrok <brianjrok@gmail.com>
2022-02-02 16:29:12 -0500

    updated user and event controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :show, :new, :create]
+  resources :users, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_210213) do
+ActiveRecord::Schema.define(version: 2022_02_02_211253) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
