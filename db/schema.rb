89eba3282ca2fd98e1035d1e558ba5a7bb484f65
brianjrok <brianjrok@gmail.com>
2022-02-02 16:42:40 -0500

    added Event name to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_211253) do
+ActiveRecord::Schema.define(version: 2022_02_02_214211) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_211253) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
