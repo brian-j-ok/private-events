6066877c2fab500b9af5d2743fe3346d31aeec7c
brianjrok <brianjrok@gmail.com>
2022-02-02 21:27:17 -0500

    fixed routing (member) and updated show for event

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'events#index'
 
-  resources :events, only: [:index, :show, :new, :create]
+  resources :events, only: [:index, :show, :new, :create] do
+    member do
+      get 'rsvp'
+      get 'cancel_rsvp'
+    end
+  end
+
   resources :users, only: [:show]
 end
--- db/migrate/20220203005946_create_rsvps.rb
+++
@@ -1,8 +0,0 @@
-class CreateRsvps < ActiveRecord::Migration[6.1]
-  def change
-    create_table :rsvps do |t|
-
-      t.timestamps
-    end
-  end
-end
